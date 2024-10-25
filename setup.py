13b7ec1f720e4a256dab500e4b065b846d821fb5
eita <>
2024-10-25 12:14:22 +0900

    py2appを導入

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+import sys
+if sys.platform == "win32":
+    import _overlapped
+    import _io._WindowsConsoleIO
+APP = ['GPTs_Tetris.py']  # tkinterアプリのスクリプトファイル名を指定
+OPTIONS = {
+    'packages': ['tkinter'],  # tkinterのみをパッケージング
+    'iconfile': 'file.icns',  
+}
+
+# high_scores.csvのパスを指定
+DATA_FILES = [('',['high_scores.csv'])]
+
+# セットアップ
+setup(
+    app=APP,
+    name='TETRIS For GPT',# アプリケーションの名前
+    data_files=DATA_FILES,
+    options={'py2app': OPTIONS},
+    setup_requires=['py2app'],
+)
